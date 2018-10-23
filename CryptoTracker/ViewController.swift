08547f7fe3f87223bc4225bdab3989a5a8ac151c
AndyChung <andychung1005@gmail.com>
2018-10-23 13:01:22 +0800

    Initialize App

---
+++ CryptoTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CryptoTracker
+//
+//  Created by 钟汇杭 on 2018/10/23.
+//  Copyright © 2018 钟汇杭. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
